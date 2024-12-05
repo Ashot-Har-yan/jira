cc2054ca68d706fce4c535bf4d79a510c1ab4e89
Ashot Harutyunyan <harutyunyanashot787@gmail.com>
2024-12-05 11:28:15 +0400

    add editIssue functional

--- src/pages/cabinet/index.js
+++ src/pages/cabinet/index.js
@@ -2,13 +2,15 @@ import { Button } from "antd";
 import { useState,useEffect } from "react";
 import { useSelector,useDispatch } from "react-redux";
 import AddIssueModal from "../../components/sheard/IssuieModal/Add";
+import EditIssueModal from "../../components/sheard/IssuieModal/Edit";
 import { fetchIssueData } from "../../state-management/slices/issues";
+import './index.css';
 
 const Cabinet=()=>{
     const dispatch = useDispatch();
     const [showModal,setShowModal] = useState(false);
-    const {data,isLoading} = useSelector((store)=>store.issues)
-    console.log(data,'store');
+    const {data,isLoading} = useSelector((store)=>store.issues);
+    const [editModalData,setEditModalData] = useState(null)
 
     useEffect(()=>{
         dispatch(fetchIssueData());
@@ -20,13 +22,35 @@ const Cabinet=()=>{
     const handleClose = ()=>{
         setShowModal(false)
     }
-
     return(
         <div>
            <Button  type="primary" onClick={handleOpenModal}>
             Create Issue
            </Button>
-           <AddIssueModal onClose = {handleClose} isOpen={showModal}/>
+          { 
+          Boolean(editModalData) &&
+           <EditIssueModal 
+           data= {editModalData}
+           isOpen={Boolean(editModalData)} 
+           onClose={()=>setEditModalData(null)}/>
+           }
+           <AddIssueModal 
+           onClose = {handleClose} 
+           isOpen={showModal}/>
+            {/* Todo */}
+           <div className="board_container">
+                  <ul>
+                    {
+                        data.map((item)=>{
+                            return(
+                            <li key = {item.taskId} onClick={()=>setEditModalData(item)}>
+                                {item.issueName}
+                            </li>)
+                        })
+                    }
+                  </ul>
+
+           </div>
         </div>
     )
 }
